7b6f0565c8faa9c4a6a26802b1b5b3950eee0836
bodysplash <bodysplash@gmail.com>
2011-12-09 14:40:26 +0100

    intermediaire

--- src/main/java/fr/arpinum/kata/videoclub/Client.java
+++ src/main/java/fr/arpinum/kata/videoclub/Client.java
@@ -1,68 +1,51 @@
 package fr.arpinum.kata.videoclub;
 
-import java.util.List;
-
 import com.google.common.collect.Lists;
 
-import fr.arpinum.kata.videoclub.Film.TypePrix;
+import java.util.List;
 
 public class Client {
 
-	private String nom;
-	private List<Location> locations = Lists.newArrayList();
+    private String nom;
+    private List<Location> locations = Lists.newArrayList();
 
-	public Client(String nom) {
-		this.nom = nom;
-	}
+    public Client(String nom) {
+        this.nom = nom;
+    }
 
-	public void ajouteLocation(Location location) {
-		locations.add(location);
-	}
+    public void ajouteLocation(Location location) {
+        locations.add(location);
+    }
 
-	public String getNom() {
-		return nom;
-	}
+    public String getNom() {
+        return nom;
+    }
 
-	public String résumé() {
-		double montantTotal = 0;
-		int pointsDeFidélités = 0;
-		String résultat = "Liste des locations pour " + getNom() + "\n";
-		for (Location location : locations) {
-			double montantCourant = 0;
+    public String résumé() {
+        String résultat = "Liste des locations pour " + getNom() + "\n";
+        for (Location location : locations) {
+            résultat += "\t" + location.getFilm().getTitre() + "\t" +
+                    String.valueOf(location.montant()) + "\n";
+        }
+        résultat += "Le montant dû est " + String.valueOf(montantTotal()) + "\n";
+        résultat += "Vous avez gagné " + String.valueOf(pointsDeFidélitésTotaux()) + " points de fidélité";
+        return résultat;
+    }
 
-			// détermine le montant pour chaque location
-			switch (location.getFilm().getTypePrix()) {
-			case NORMAL:
-				montantCourant += 2;
-				if (location.getJoursLoués() > 2)
-					montantCourant += (location.getJoursLoués() - 2) * 1.5;
-				break;
-			case NOUVEAUTE:
-				montantCourant += location.getJoursLoués() * 3;
-				break;
-			case ENFANTS:
-				montantCourant += 1.5;
-				if (location.getJoursLoués() > 3) {
-					montantCourant += (location.getJoursLoués() - 3) * 1.5;
-				}
-				break;
-			}
+    private double montantTotal() {
+        double résultat = 0;
+        for (Location location : locations) {
+            résultat += location.montant();
+        }
+        return résultat;
+    }
 
-			// ajout des points de fidélités
-			pointsDeFidélités++;
-			// ajout d'un bonus pour location de deux jours d'une nouveauté
-			if (location.getFilm().getTypePrix() == TypePrix.NOUVEAUTE
-					&& location.getJoursLoués() > 1)
-				pointsDeFidélités++;
-			
-			// montre le résultat pour cette location
-			résultat += "\t" + location.getFilm().getTitre() + "\t" + 
-				String.valueOf(montantCourant) + "\n";
-			montantTotal += montantCourant;
-		}
-		// ajout des lignes de footer
-		résultat += "Le montant dû est " + String.valueOf(montantTotal) + "\n";
-		résultat += "Vous avez gagné " + String.valueOf(pointsDeFidélités) + " points de fidélité";
-		return résultat;
-	}
+    private int pointsDeFidélitésTotaux() {
+        int pointsDeFidélités = 0;
+        for (Location location : locations) {
+            pointsDeFidélités += location.pointsDeFidélité();
+        }
+        return pointsDeFidélités;
+    }
+
 }
--- src/main/java/fr/arpinum/kata/videoclub/Location.java
+++ src/main/java/fr/arpinum/kata/videoclub/Location.java
@@ -2,7 +2,39 @@ package fr.arpinum.kata.videoclub;
 
 public class Location {
 
-	private Film film;
+    double montant() {
+        double résultat = 0;
+        // détermine le montant pour chaque location
+        switch (getFilm().getTypePrix()) {
+        case NORMAL:
+            résultat += 2;
+            if (getJoursLoués() > 2)
+                résultat += (getJoursLoués() - 2) * 1.5;
+            break;
+        case NOUVEAUTE:
+            résultat += getJoursLoués() * 3;
+            break;
+        case ENFANTS:
+            résultat += 1.5;
+            if (getJoursLoués() > 3) {
+                résultat += (getJoursLoués() - 3) * 1.5;
+            }
+            break;
+        }
+        return résultat;
+    }
+
+    int pointsDeFidélité() {
+        int résultat = 1;
+        // ajout d'un bonus pour location de deux jours d'une nouveauté
+        if (getFilm().getTypePrix() == Film.TypePrix.NOUVEAUTE
+                && getJoursLoués() > 1) {
+            résultat += 1;
+        }
+        return résultat;
+    }
+
+    private Film film;
 	private int joursLoués;
 	
 	public Location(Film film, int joursLoués) {
